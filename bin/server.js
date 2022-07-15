678335b3918dba1435a95d82bda61ff9cc5a31fc
leo-demetrio <leocdemetrio@gmail.com>
2022-07-15 18:39:10 -0300

    refactor: separate and create files routes
    Create files index-route and product-route

--- bin/server.js
+++ bin/server.js
@@ -4,15 +4,10 @@ const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 
-
-
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-
-
-
 
 server.listen(port);
 server.on('error', onError);
