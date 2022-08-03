833cdff0286babb22bf7c8dcebc937037dbc71d5
leo-demetrio <leocdemetrio@gmail.com>
2022-08-03 06:19:05 -0300

    feat: Connect mongo atlas

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+
+mongoose.connect(`mongodb+srv://petshop_leop:mongopetshop@cluster0.rahrifh.mongodb.net/?retryWrites=true&w=majority`)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
