74b89326a77d624feb4705a35d6151d5469c6d06
leo-demetrio <leocdemetrio@gmail.com>
2022-08-27 14:25:39 -0300

    Feat: Model and Crud Product created

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,7 +4,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
-router.get('/', controller.get)
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/tag/:tag', controller.getByTag);
+router.get('/admin/:id', controller.getById);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
